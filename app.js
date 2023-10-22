c1a01c98f3f142800736e23d889a877f3c5dd931
adesh <adesh.sonawane@morfsys.com>
2023-10-22 23:26:06 +0530

    second push

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+const heading = (<h1 className="heading">I'm a react element</h1>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
