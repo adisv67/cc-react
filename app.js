ac12e51dfc65c3480ebded6c5dd87385af42b29c
adesh <adesh.sonawane@morfsys.com>
2023-10-24 12:42:22 +0530

    component inside element

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
-
-const heading = (<h1 className="heading">I'm a react element</h1>);
-
+import  ReactDOM  from "react-dom/client";
+const HeadingComp = () => {
+    return (
+    <div>
+        <h1 className="heading">I'm a react Component</h1>
+    </div>);
+}
+const heading = (
+    <div>
+        <h1 className="heading">I'm a react element</h1>,
+        <HeadingComp/>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
