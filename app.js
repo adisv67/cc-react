1c53cf286a90e5628a584c2d520020d3c27f68b8
adesh <adesh.sonawane@morfsys.com>
2023-10-24 12:47:07 +0530

    fragment learn

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
 const HeadingComp = () => {
     return (
-    <div>
+  
         <h1 className="heading">I'm a react Component</h1>
-    </div>);
+    )
 }
 const heading = (
-    <div>
-        <h1 className="heading">I'm a react element</h1>,
+    <>
+        <h1 className="heading">I'm a react element</h1>
         <HeadingComp/>
-    </div>
+    </>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
